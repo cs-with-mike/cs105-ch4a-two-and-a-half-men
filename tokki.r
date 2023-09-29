561caaf41366d9df6ca3908787980c2e693edf15
Landon John Mueller-Amaral <113473916+ljmamaral@users.noreply.github.com>
2023-09-28 20:29:10 -0700

    Variables and Lookup Function

--- tokki.r
+++ tokki.r
@@ -2,16 +2,31 @@
 
 #/* Global declarations */ 
 
-#/* Variables */ 
+# Variables
+charClass <- NULL  # Initialize to NULL
+lexeme <- character(100)  # Initialize as a character vector of length 100
+nextChar <- NULL  # Initialize to NULL
+lexLen <- NULL  # Initialize to NULL
+token <- NULL  # Initialize to NULL
+nextToken <- NULL  # Initialize to NULL
+in_fp <- NULL  # Initialize to NULL
 
+# Character classes
+LETTER <- 0
+DIGIT <- 1
+UNKNOWN <- 99
 
-#/* Function declarations */ 
-
-
-#/* Character classes */ 
-
-
-#/* Token codes */ 
+# Token codes
+INT_LIT <- 10
+IDENT <- 11
+ASSIGN_OP <- 20
+ADD_OP <- 21
+SUB_OP <- 22
+MULT_OP <- 23
+DIV_OP <- 24
+LEFT_PAREN <- 25
+RIGHT_PAREN <- 26
+EOF <- -1
 
 #/******************************************************/ 
 #/* main driver */ 
@@ -20,6 +35,42 @@
 
 #/*****************************************************/ 
 #/* lookup - a function to lookup operators and parentheses and return the token */ 
+
+# Look up table
+# Returns null if not here meaning the file is empty
+lookup <- function(ch) {
+  switch(ch,
+         "(" = {
+           print("addChar...")
+           nextToken <<- LEFT_PAREN
+         },
+         ")" = {
+           print("addChar...")
+           nextToken <<- RIGHT_PAREN
+         },
+         "+" = {
+           print("addChar...")
+           nextToken <<- ADD_OP
+         },
+         "-" = {
+           print("addChar...")
+           nextToken <<- SUB_OP
+         },
+         "*" = {
+           print("addChar...")
+           nextToken <<- MULT_OP
+         },
+         "/" = {
+           print("addChar...")
+           nextToken <<- DIV_OP
+         },
+         default = {
+           print("addChar...")
+           nextToken <<- EOF
+         }
+  )
+  return(nextToken)
+}
 
 #/*****************************************************/ 
 #/* addChar - a function to add nextChar to lexeme */
