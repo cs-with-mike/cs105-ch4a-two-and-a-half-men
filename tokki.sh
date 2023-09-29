a7cf701bf33f773ce560df208beb2e1992846e84
LoganTown <105823728+LoganTown@users.noreply.github.com>
2023-09-28 19:37:53 -0700

    Update tokki.sh with some R environment commands

--- tokki.sh
+++ tokki.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Authors:
-#  - Boaty McBoatface, bmcboatface@westmont.edu
+#  - Logan Towne, ltowne@westmont.edu
 #  - Thomas McTankengine, tmctankengine@westmont.edu
 #  - Ovenly McEasybake, omceasybake@westmont.edu
 
 # Run any commands necessary to set up your language's runtime environment here.
 # If the runtime is expected to be present on Ubuntu by default, then do nothing.
 echo "Setting up runtime ..."
+sudo apt update
+sudo apt install r-base r-base-dev -y
+R
 
 # I'm checking Python interpreter's version here just as a placeholder.
 python3 --version # TODO: YOUR RUNTIME SETUP HERE.
