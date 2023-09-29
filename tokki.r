bdd20803a6d0dac00197e595c811aa00df139461
LoganTown <105823728+LoganTown@users.noreply.github.com>
2023-09-28 18:41:46 -0700

    Update tokki.r

--- tokki.r
+++ tokki.r
@@ -1,1 +1,49 @@
-//idk comment whatever
+#/* front.c - a lexical analyzer system for simple arithmetic expressions */ 
+
+#/* Global declarations */ 
+
+#/* Variables */ 
+
+
+#/* Function declarations */ 
+
+
+#/* Character classes */ 
+
+
+#/* Token codes */ 
+
+#/******************************************************/ 
+#/* main driver */ 
+
+#/* Open the input data file and process its contents */ 
+
+#/*****************************************************/ 
+#/* lookup - a function to lookup operators and parentheses and return the token */ 
+
+#/*****************************************************/ 
+#/* addChar - a function to add nextChar to lexeme */
+
+#/*****************************************************/ 
+#/* getChar - a function to get the next character of 
+
+#/*****************************************************/ 
+#/* getNonBlank - a function to call getChar until it returns a non-whitespace character */ 
+
+#/ *****************************************************/ 
+#/* lex - a simple lexical analyzer for arithmetic expressions */ 
+
+#/* Parse identifiers */ 
+
+#/* Parse integer literals */ 
+
+#/* Parentheses and operators */ 
+
+#/* EOF */ 
+
+#/* End of switch */ 
+
+#/* End of function lex */
+
+
+
