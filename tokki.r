9e1849fefa358cc01e15e522c6033b484f96e94a
logantown <ltowne@westmont.edu>
2023-09-29 14:06:16 -0700

    setup or test cases not for file input

--- tokki.r
+++ tokki.r
@@ -1,22 +1,15 @@
-#front.c - a lexical analyzer system for simple arithmetic expressions
-
-#Global declarations
-
-# Variables
-charClass <- "" 
-lexeme <- character(100)  # Initialize as a character vector of length 100
-nextChar <- ""  
+charClass <- ""
+lexeme <- character(100)  
+nextChar <- ""
 lexLen <- 0
 token <- 0
-nextToken <- 0  
+nextToken <- 0
 in_fp <- NULL  
 
-# Character classes
 LETTER <- "LETTER"
 DIGIT <- "DIGIT"
 UNKNOWN <- "UNKNOWN"
 
-# Token codes
 INT_LIT <- "INT_LIT"
 IDENT <- "IDENT"
 ASSIGN_OP <- "ASSIGN_OP"
@@ -28,48 +21,36 @@ LEFT_PAREN <- "LEFT_PAREN"
 RIGHT_PAREN <- "RIGHT_PAREN"
 EOF <- "EOF"
 
-#/lookup - a function to lookup operators and parentheses and return the token
-
-# Look up table WORKS
 lookup <- function(ch) {
   switch(ch,
          '(' = {
-           print("addChar...")
            nextToken <<- LEFT_PAREN
          },
          ')' = {
-           print("addChar...")
            nextToken <<- RIGHT_PAREN
          },
          '+' = {
-           print("addChar...")
            nextToken <<- ADD_OP
          },
          '-' = {
-           print("addChar...")
            nextToken <<- SUB_OP
          },
          '*' = {
-           print("addChar...")
            nextToken <<- MULT_OP
          },
          '/' = {
-           print("addChar...")
            nextToken <<- DIV_OP
          },
          '=' = {
-           print("addChar...")
            nextToken <<- ASSIGN_OP
          },
          default = {
-           print("addChar...")
            nextToken <<- EOF
          }
   )
   return(nextToken)
 }
 
-#addChar - a function to add nextChar to lexeme WORKS
 addChar <- function() {
   if (lexLen <= 98) {
     lexeme[lexLen + 1] <- nextChar
@@ -80,17 +61,10 @@ addChar <- function() {
   }
 }
 
-#getChar - a function to get the next character of input and determine its character class WORKS
 getChar <- function() {
-  # Open the file connection if it's not already open
-  if (is.null(in_fp)) {
-    in_fp <<- file("sample.tk", "r")
-  }
-  
-  # Read a single character from the file connection
-  nextChar <<- readChar(in_fp, 1)
-  
-  if (nextChar != "") {
+  if (length(char_iterator) > 0) {
+    nextChar <<- char_iterator[1]
+    char_iterator <<- char_iterator[-1]
     if (nextChar %in% c('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')) {
       charClass <<- DIGIT
     } else if (grepl("[A-Za-z]", nextChar)) {
@@ -100,20 +74,19 @@ getChar <- function() {
     }
   } else {
     charClass <<- EOF
+    nextChar <<- ""
   }
 }
 
-#getNonBlank - a function to call getChar until it returns a non-whitespace character SHOULDWORK
 getNonBlank <- function() {
   while (grepl("\\s", nextChar)) {
     getChar()
   }
 }
 
-#lex - a simple lexical analyzer for arithmetic expressions
 lex <- function() {
   lexLen <<- 0
-  lexeme <<- character(100)  # Reset lexeme
+  lexeme <<- character(100)  
   
   getNonBlank()
   
@@ -146,25 +119,33 @@ lex <- function() {
          }
   )
   
-  cat(paste("Next token is:", nextToken, ", Next lexeme is ", paste(lexeme, collapse = ""), "\n"))
+  token_name <- switch(
+    nextToken,
+    LEFT_PAREN = "LEFT_PAREN",
+    RIGHT_PAREN = "RIGHT_PAREN",
+    ADD_OP = "ADD_OP",
+    SUB_OP = "SUB_OP",
+    MULT_OP = "MULT_OP",
+    DIV_OP = "DIV_OP",
+    ASSIGN_OP = "ASSIGN_OP",
+    IDENT = "IDENT",
+    INT_LIT = "INT_LIT",
+    EOF = "EOF",
+    UNKNOWN = "UNKNOWN"
+  )
+  
+  cat(paste("Next token is:", token_name, "| Next lexeme is", paste(lexeme, collapse = ""), "\n"))
   return(nextToken)
 }
 
 
-# main driver *
-#Open the input data file and process its contents 
-if (file.exists("sample.tk")) {
-  in_fp <- file("sample.tk", "r")
-  getChar()
-  repeat {
-    if (lex() == EOF) {
-      break
-    }
+input_string <- "(sum + 47) / total"
+
+char_iterator <- strsplit(input_string, "")[[1]]
+
+getChar()
+repeat {
+  if (lex() == EOF) {
+    break
   }
-  close(in_fp)
-} else {
-  cat("ERROR - cannot open front.in\n")
 }
-
-
-
