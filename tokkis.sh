c0c7ce5e5767bd47db995cda9f60f55519eacc26
Landon John Mueller-Amaral <113473916+ljmamaral@users.noreply.github.com>
2023-10-06 22:03:04 -0700

    Create tokkis.sh and copied all tokki.sh code over

---
+++ tokkis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Authors:
+#  - Logan Towne, ltowne@westmont.edu
+#  - Landon Amaral, lamaral@westmont.edu
+#  - Eli Tiao, jtiao@westmont.edu
+
+# Run any commands necessary to set up your language's runtime environment here.
+# If the runtime is expected to be present on Ubuntu by default, then do nothing.
+echo "Setting up runtime ..."
+sudo apt update
+sudo apt install r-base r-base-dev -y
+R
+
+# Run your Tokki lexer, passing in the first command line argument directly to the lexer.
+# Any output to STDOUT should be directed to a text output file titled "out.txt."
+echo "Running Tokki ... output file is out.txt"
+
+# This will run out tokki file and output it on out.txt
+Rscript tokki.r $1 > out.txt
